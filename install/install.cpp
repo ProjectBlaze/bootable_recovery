d4165e0b0326ff0f49eb6affdd77a2743e0a5c12
RDS_007 <ramandeep392@gmail.com>
2022-05-09 14:23:33 +0530

    Revert "recovery: Disable downgrade check"
    This reverts commit 49c4113887a96cfefb2af31b9a5a71af97612140.

--- install/install.cpp
+++ install/install.cpp
@@ -166,7 +166,7 @@ static bool CheckAbSpecificMetadata(const std::map<std::string, std::string>& me
   }
 
   // Check for downgrade version.
-  /*int64_t build_timestamp =
+  int64_t build_timestamp =
       android::base::GetIntProperty("ro.build.date.utc", std::numeric_limits<int64_t>::max());
   int64_t pkg_post_timestamp = 0;
   // We allow to full update to the same version we are running, in case there
@@ -186,7 +186,7 @@ static bool CheckAbSpecificMetadata(const std::map<std::string, std::string>& me
       LOG(ERROR) << "Downgrade package must have a pre-build version set, not allowed.";
       return false;
     }
-  }*/
+  }
 
   return true;
 }
